d7372caaf27c548d88092208630725d8d276a655
EXsunshine <105021057@live.asia.edu.tw>
2016-10-17 16:02:49 +0800

    ex  hw  not  fin

--- src/ex/ex01_1050210XX.java
+++ src/ex/ex01_1050210XX.java
@@ -4,11 +4,18 @@
  * Date: 2016/10/17
  * Author: 1050210XX 周永振老師
  */
-
+import java.util.*;
 public class ex01_1050210XX {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+Scanner scn = new Scanner(System.in);
+int a = scn.nextInt();
+double sum = 0;
+while(a<0){
+	sum+=a;
+	
+}
 
 	}
 
--- src/ex/ex02_1050210XX.java
+++ src/ex/ex02_1050210XX.java
@@ -4,12 +4,26 @@
  * Date: 2016/10/17
  * Author: 1050210XX 周永振老師
  */
-
+import java.util.*;
 public class ex02_1050210XX {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		boolean run = true;
+		int a = scn.nextInt();
+		int b = scn.nextInt();
+		System.out.println(a+"*"+b+"="+a*b);
+		while(run = true){
+		System.out.print("是否繼續(Y/n):");
+		char fu = scn.next().charAt(0);
+		if(fu == 'n' || fu == 'N'){
+			break;
+		}else{
+			a = scn.nextInt();
+			b = scn.nextInt();
+			System.out.println(a+"*"+b+"="+a*b);
+		}
+		}
 	}
-
 }
